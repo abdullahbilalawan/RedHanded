1f350b17e16ff81b2b243f84cd9f493ef8f95a14
Aliza Qamer <aqamer.bese19seecs@seecs.edu.pk>
2021-08-25 20:02:47 +0500

    sidebar changes

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = (props) => {
   console.log.apply(props.history);
   return (
     <>
-<<<<<<< HEAD
+
     <div className="homecontainer">
      
   <Baner
@@ -21,15 +21,7 @@ const HomePage = (props) => {
       </>
   
    
-=======
-      <div className="homecontainer">
-        <div className="homepage">
-          <Baner title="Red Handed" subtitle="AI invigilation" />
-        </div>
-      </div>
-    </>
->>>>>>> 634b9483e147303073c92acd989267b88c58ee29
-  );
-};
+  )}
+     
 
 export default HomePage;
